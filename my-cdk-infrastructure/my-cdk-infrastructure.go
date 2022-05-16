8c9ca723c81664d55be63356d88c2546b1e05cfd
Erik <45368867+eriklz@users.noreply.github.com>
2022-05-16 15:21:35 +0200

    feat: add EC2 instance role allow login to machine

--- my-cdk-infrastructure/my-cdk-infrastructure.go
+++ my-cdk-infrastructure/my-cdk-infrastructure.go
@@ -2,34 +2,45 @@ package main
 
 import (
 	"os"
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	ec2 "github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	iam "github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	//"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
 
 func main() {
-	app := awscdk.NewApp(nil)
+	app := cdk.NewApp(nil)
 
-	stack := awscdk.NewStack(app, jsii.String("my-stack"), &awscdk.StackProps {
+	stack := cdk.NewStack(app, jsii.String("my-stack"), &cdk.StackProps {
 		Env: env(),
 	})
 
-	vpc := awsec2.Vpc_FromLookup(stack, jsii.String("my-vpc"), &awsec2.VpcLookupOptions {
+	var policies = []iam.IManagedPolicy {
+		iam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("AmazonSSMManagedInstanceCore")),
+	}
+
+	role := iam.NewRole(stack, jsii.String("ec2-role"), &iam.RoleProps{
+		AssumedBy: iam.NewServicePrincipal(jsii.String("ec2.amazonaws.com"), nil),
+		ManagedPolicies: &policies,
+	})
+
+	vpc := ec2.Vpc_FromLookup(stack, jsii.String("my-vpc"), &ec2.VpcLookupOptions {
 		IsDefault: jsii.Bool(true),
 	})
 
-	awsec2.NewInstance(stack, jsii.String("my-ec2"), &awsec2.InstanceProps {
-		InstanceType: awsec2.InstanceType_Of(awsec2.InstanceClass_BURSTABLE2, awsec2.InstanceSize_MICRO),
-		MachineImage: awsec2.MachineImage_LatestAmazonLinux(nil),
+	ec2.NewInstance(stack, jsii.String("my-ec2"), &ec2.InstanceProps {
+		InstanceType: ec2.InstanceType_Of(ec2.InstanceClass_BURSTABLE2, ec2.InstanceSize_MICRO),
+		MachineImage: ec2.MachineImage_LatestAmazonLinux(nil),
+		Role: role,
 		Vpc: vpc,
 	})
 
 	app.Synth(nil)
 }
 
-func env() *awscdk.Environment {
-	return &awscdk.Environment{
+func env() *cdk.Environment {
+	return &cdk.Environment{
 	  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
 	  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
 	}
